6f2c10308504088fb588b28eca62420a5b21a0a8
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-29 04:23:29 +0200

    Implemented selection sort algorithm and provided it's time complexities

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers.
+ * @array: Pointer an array of integers.
+ * size: Size of the array.
+ *
+ * Return: Nothing.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, num;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		num = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[num])
+				num = j;
+			if (num != i)
+				swap(&array[num], &array[i]);
+		}
+		print_array(array, size);
+
+	}
+}
+
+/**
+ * swap - helper function to swap array elements.
+ * @a: pointer to integer value.
+ * @b: pointer to integer value.
+ *
+ * Return: Void.
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+		0x1B. C - Sorting algorithms & Big O
+	This Repository comprises Sorting algorithms and Big O Notations.
+	In this project I implement different sorting algorithms including selection sort, Bubble sort, insertion sort, Merge sort
+	and Quick sort algorithms  and I  provide their time complexities in in the best case, average case, and the worst case.
