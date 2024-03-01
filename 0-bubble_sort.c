2ffd8cb073e7cb8e5ebf5eb68338d5ab5647e34d
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-01 10:47:26 +0200

    checker

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, temp;
 	int sorted;
 
 	if (size < 2)
@@ -22,7 +22,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 				sorted = 1;
 			}
