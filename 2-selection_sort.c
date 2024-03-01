de7ce1ff408ada277cfc46b095e08a63db6914ab
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-01 12:22:53 +0200

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,28 +2,31 @@
 /**
  * selection_sort - sorts an array of integers.
  * @array: Pointer an array of integers.
- * size: Size of the array.
+ * @size: Size of the array.
  *
  * Return: Nothing.
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, num;
-
-	if (size < 2)
-		return;
+	size_t i, j, small_num;
+	size_t temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		num = i;
+		/*searching for smaller number in the array*/
+		small_num = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[num])
-				num = j;
-			if (num != i)
-				swap(&array[num], &array[i]);
+			if (array[j] < array[small_num])
+				small_num = j;
 		}
-		print_array(array, size);
-
+		/*if the found small number is not at the start of the unsorted array swap*/
+		if (small_num != i)
+		{
+			temp = array[small_num];
+			array[small_num] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 	}
 }
