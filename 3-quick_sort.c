d2e07e4e16e1a02d5ac2a45ddd4284e044736d6d
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-02 16:22:20 +0200

    edge case

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -82,7 +82,7 @@ void quick_sort_recursive(int *array, int first, int last, size_t size)
 */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
 	quick_sort_recursive(array, 0, size - 1, size);
