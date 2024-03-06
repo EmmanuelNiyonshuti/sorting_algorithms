c87ebdf9b8d5dba937d419be164eaa10ee573855
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-06 16:14:24 +0200

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,4 +9,28 @@
  */
 void shell_sort(int *array, size_t size)
 {
+	size_t i, j, gap;
+	int tmp;
 
+	gap = 1;
+	while (gap < size / 3)
+	{
+		gap = (gap * 3) + 1;
+	}
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			j = i;
+			while (j >= gap && array[j - gap] > tmp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = tmp;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+}
